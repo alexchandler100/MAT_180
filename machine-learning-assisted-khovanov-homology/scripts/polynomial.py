927b6f96e77d27ccb17fee803b1b5de82dc30f09
Keer Ni <knni@ucdavis.edu>
2022-12-06 15:52:47 -0800

    extract find_max_min_row and find_max_min_column in .py files
    apply find_max_min_row and find_max_min_col to reduce repetitive code blocks

--- machine-learning-assisted-khovanov-homology/scripts/polynomial.py
+++ machine-learning-assisted-khovanov-homology/scripts/polynomial.py
@@ -9,6 +9,7 @@ def generate_monomials_eq(n,k):
         for i in range(k+1):
             for j in generate_monomials_eq(n-1,k-i):
                 yield [i,] + j
+
 def generate_monomials_leq(n,k): 
     L = []
     for i in range(k+1):
@@ -16,6 +17,7 @@ def generate_monomials_leq(n,k):
         Lh.sort(reverse = True)
         L += Lh
     return L
+
 def add_poly_terms(X,k):
     n = len(X[0])
     mons = generate_monomials_leq(n,k)

e0b2ac126cb051a0291bfec33ceda6f6ed255d14
Clément Weinreich <clement@weinreich.fr>
2022-12-07 18:59:01 -0800

    Fix p_sample and reverse process

--- colorized/scripts/reverse_process.py
+++ colorized/scripts/reverse_process.py
@@ -2,9 +2,13 @@ import torch
 import sys
 sys.path.append('../')
 from scripts import util
+import gc
+
+
 
 # calls the model to predict the noise in the image and return the denoised image
 # it is the algorithm 2 in ddpm paper
+@torch.no_grad()
 def p_sample(model, x, cond_x, t, t_index, constantDiffusionTerms):
     # match batch timesteps to various corresponding values 
     sqrt_alphas_inverse_t = util.extract_t_th_value_of_list(constantDiffusionTerms.sqrt_alphas_inverse, t, x.shape)
@@ -13,7 +17,6 @@ def p_sample(model, x, cond_x, t, t_index, constantDiffusionTerms):
     
     # calculate the (noise) parameterized mean using our UNet
     parameterized_model_mean = sqrt_alphas_inverse_t * (x - betas_t * model(x, cond_x, t) / sqrt_one_minus_alphas_cumulative_prods_t)
-
     # If we have completed the entire reverse process, return the parameterized mean
     # else generate random noise (epsilon), scale by sqrt(beta) and add to model mean 
     # (reparametrization trick)
@@ -26,6 +29,7 @@ def p_sample(model, x, cond_x, t, t_index, constantDiffusionTerms):
     
 
 # same but start from pure noise x_T and loop until getting a new x_0
+@torch.no_grad()
 def p_sample_loop(model, shape, device, timesteps, cond_x, constantDiffusionTerms):
     #shape=(batch_size, channels, image_width, image_height)
     batch_size = shape[0]
@@ -36,6 +40,7 @@ def p_sample_loop(model, shape, device, timesteps, cond_x, constantDiffusionTerm
 
     # for each timestep until 0, remove some noise
     for t in reversed(range(0, timesteps)):
+        print(t)
 
         # copy the timestep t into a vector of size batch_size
         batch_sized_timestep = torch.full((batch_size,), t, device=device) 
@@ -44,7 +49,7 @@ def p_sample_loop(model, shape, device, timesteps, cond_x, constantDiffusionTerm
                                 noisy_image, cond_x,
                                 batch_sized_timestep, t,
                                 constantDiffusionTerms)
-        if(t%round(timesteps/8)):
+        if(t%30==0):
             print("saving!")
             noisy_images.append(noisy_image.detach().cpu().numpy())
     

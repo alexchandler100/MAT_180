685eb3d3a0b0dedc52f06b475a25a8933395ae3d
Ayush Chakravarthy <ayush.k.chakravarthy@gmail.com>
2022-12-07 21:01:04 -0800

    fixed some issues

--- learning-robust-representations-learned-through-distribution-shift/submit_resnet_cifar10.sh
+++ learning-robust-representations-learned-through-distribution-shift/submit_resnet_cifar10.sh
@@ -12,9 +12,9 @@ conda activate mem
 seed=$1
 name=$2
 
-python3 train_resnet_cifar10_oracle_db.py \
+python3 train_resnet_on_cifar10.py \
 	--buffer_model_path=./pretrained_models/byol_cifar10.ckpt --buffer_model_dict=./pretrained_models/byol_cifar10.json --attn_dropout=0.1 \
 	--attn_retrieval=reps_for_both --batch_size=256 --buffer_path=./data_buffer/byol_cifar10.pt --cross_dim_head=32 \
 	--cross_heads=1 --depth=2 --eval_batches=3 --eval_every_steps=5 --ff_dropout=0.1 --latent_dim=512 \
 	--latent_heads=2 --name=${name} --num-workers=1 --num_latents=10 --root_dir='~/data/' --seed=${seed} \
-	--retrieval_access=oracle_sampled --epochs 300 --no_labels_from_reps
+	--retrieval_access=knn --epochs 300 --no_labels_from_reps
